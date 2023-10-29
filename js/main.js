33d73519134140eee84c4fec461d0b2561620ae2
chojeongeun <juy0816@naver.com>
2023-10-29 11:04:43 +0900

    prev 기능 추가

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@ const main = document.querySelector('main');
 const panel = main.querySelector('.panel');
 const [prev, next] = main.querySelectorAll('span');
 
+prev.addEventListener('click', () => {
+	new Anime(
+		panel,
+		{ left: '-0%' },
+		{
+			duration: 500,
+			callback: () => {
+				panel.prepend(panel.lastElementChild);
+				panel.style.left = '-100%';
+			},
+		}
+	);
+});
+
 next.addEventListener('click', () => {
 	new Anime(
 		panel,
 		{ left: '-200%' },
 		{
-			duration: 2000,
+			duration: 500,
 			callback: () => {
 				panel.append(panel.firstElementChild);
 				panel.style.left = '-100%';
